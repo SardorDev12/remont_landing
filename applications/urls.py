4095aa03fa3f361a643f73d5cf315094a928c31d
SardorFargodOgli <sardoribnfarhod@gmail.com>
2024-01-01 15:08:31 +0500

    minor fix

--- applications/urls.py
+++ applications/urls.py
@@ -1,8 +1,8 @@
 # app_1/urls.py
 from django.urls import path
-from .views import ApplicationCreateView, ApplicationListCreateView
+from .views import ApplicationCreate, ApplicationListCreateView
 
 urlpatterns = [
     path('application/', ApplicationListCreateView.as_view(), name='application-list-create'),
-    path('application/create/', ApplicationCreateView.as_view(), name='application-create'),
+    path('application/create/', ApplicationCreate.as_view(), name='application-create'),
 ]
