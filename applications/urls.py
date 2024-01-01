0c5d2525a32ff6272053898c6e7dd4c78e0080c1
SardorFargodOgli <sardoribnfarhod@gmail.com>
2024-01-01 14:38:20 +0500

    minor fix

--- applications/urls.py
+++ applications/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ApplicationCreateView, ApplicationListCreateView
 
 urlpatterns = [
-    path('applications/', ApplicationListCreateView.as_view(), name='application-list-create'),
+    path('application/', ApplicationListCreateView.as_view(), name='application-list-create'),
     path('application/create/', ApplicationCreateView.as_view(), name='application-create'),
 ]
--- applications/views.py
+++ applications/views.py
@@ -2,16 +2,11 @@ from rest_framework import generics
 from rest_framework.generics import ListCreateAPIView
 from .models import Application
 from .serializers import ApplicationSerializer
-from django.http import HttpResponse
 
-def ApplicationCreate(req):
-    return HttpResponse("Create api")
+class ApplicationCreate(generics.CreateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
 
-
-# class ApplicationCreate(generics.CreateAPIView):
-#     queryset = Application.objects.all()
-#     serializer_class = ApplicationSerializer
-
-# class ApplicationListCreateView(ListCreateAPIView):
-#     queryset = Application.objects.all()
-#     serializer_class = ApplicationSerializer
+class ApplicationListCreateView(ListCreateAPIView):
+    queryset = Application.objects.all()
+    serializer_class = ApplicationSerializer
