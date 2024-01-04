bd6813eb0b19910212e55ccb4e94641735a12258
SardorFargodOgli <sardoribnfarhod@gmail.com>
2024-01-04 12:51:51 +0500

    change in app url

--- applications/urls.py
+++ applications/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ApplicationCreate, ApplicationListCreateView
 
 urlpatterns = [
-    path('application/', ApplicationListCreateView.as_view(), name='application-list-create'),
-    path('application/create/', ApplicationCreate.as_view(), name='application-create'),
+    path('', ApplicationListCreateView.as_view(), name='application-list-create'),
+    path('create/', ApplicationCreate.as_view(), name='application-create'),
 ]
