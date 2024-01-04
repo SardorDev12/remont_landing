edd71e282c7f9a6453d5b5cdd3d3244234ccf16d
SardorFargodOgli <sardoribnfarhod@gmail.com>
2024-01-04 12:10:50 +0500

    minor fixces

--- comments/admin.py
+++ comments/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Comment
 
-# Register your models here.
-admin.site.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name','job')
+    search_fields = ('name',)
+
+admin.site.register(Comment, CommentAdmin)
--- comments/urls.py
+++ comments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CommentCreate,CommentListAPIView
 
 urlpatterns = [
-    path('', CommentListAPIView.as_view(), name='application-list-create'),
-    path('create/', CommentCreate.as_view(), name='application-create'),
+    path('', CommentListAPIView.as_view(), name='comments-list-create'),
+    path('create/', CommentCreate.as_view(), name='comment-create'),
 ]
