b2a474f4347f0a1fefd4cf2c23e6a372b20594f0
SardorFargodOgli <sardoribnfarhod@gmail.com>
2024-01-04 16:45:49 +0500

    minor fix

--- remont_site/settings.py
+++ remont_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7(hngw$)fyqgud)0!_!j8&z+0!n37l3%790)c^d9ll=!%i-u^f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
