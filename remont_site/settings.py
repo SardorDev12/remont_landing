42c67f1702b2b883f30fa5f531280e6214f6448c
SardorFargodOgli <sardoribnfarhod@gmail.com>
2024-01-04 15:01:36 +0500

    set up cors headers

--- remont_site/settings.py
+++ remont_site/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "applications",
     'comments',
 
-    'rest_framework'
+    'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -52,9 +53,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'remont_site.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
